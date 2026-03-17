47eb8d0209b4ab4301f9335cb946e92ade3644fc
4245877 <alner4245877@gmail.com>
2026-03-17 13:25:45 +0200

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://4245877.github.io',
-  base: '/4245877-3d-drukarnya/',
+  base: '/3D-Drukarnya',
   trailingSlash: 'always',
 });
